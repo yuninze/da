5d7f358be361e519bc789e91129c6e4a6f4452f3
yuninze <gajeongsick.gmail.com>
2022-12-05 17:55:51 +0900

    works

--- app.py
+++ app.py
@@ -11,22 +11,28 @@ def get_data(path="c:/code/f.csv"):
 
 def main():
     st.title("trial")
-    features=st.sidebar.selectbox("")
-    st.write(f.tail(5))
-    st.subheader(f"Fuck1")
-    fg,ax=plt.subplots(1,1)
-    np.log(f.ng.dropna()).hist(alpha=.6,ax=ax)
-    ax.set_title(f"f::ng")
-    st.pyplot(fg)
+    features_list=["show samples","fuck","ng"]
+    features=st.sidebar.selectbox("features_selectbox",features_list)
+    st.header(f"{features=} yeonsoup")
+    if features==features_list[0]:
+        show("show samples")
+    elif features==features_list[1]:
+        show("fuck")
+    elif features==features_list[2]:
+        show("ng")
 
-def show():
+def show(string):
     f=get_data()
-    if st.sidebar.checkbox("show samples"):
+    if string=="show samples":
         st.subheader("samples")
-        st.dataframe(f.sample(10))
-    if st.sidebar.checkbox("fuck"):
+        st.dataframe(f.sample(5))
+    elif string=="fuck":
+        st.subheader("...go see the terminal")
         print(f"fuck selected")
-
+    elif string=="ng":
+        fg,ax=plt.subplots(1,1)
+        np.log(f.ng.dropna()).hist(alpha=.6,ax=ax)
+        st.pyplot(fg)
 
 # if st.checkbox(f"Show dropna-ed samples"):
 #     st.subheader("Row samples")
@@ -38,3 +44,5 @@ def show():
 # st.radio("radiobutton",
 #     options=("選択肢1", "選択肢2", "選択肢3"))
 
+if __name__=="__main__":
+    main()
